7b4106ac2c7938e06ea0852522cf24c743af0132
janpaul <janpaul@hey.com>
2023-01-26 15:20:19 +0100

    button actually does something

--- components/button.tsx
+++ components/button.tsx
@@ -1,1 +1,11 @@
-export const Button = () => <button>foo</button>
+import type { ReactNode } from "react"
+
+interface Props {
+  children: ReactNode
+  onClick: () => void
+}
+export const Button = ({ children, onClick }: Props) => (
+  <button onClick={onClick} className="uppercase">
+    {children}
+  </button>
+)
